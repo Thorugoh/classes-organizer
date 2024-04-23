cb39545b7903ec636d6e66594fbfb960d38429da
Victor Hugo <pavictorhugo@gmail.com>
2024-04-23 16:07:00 -0300

    Add multiple times per day

--- src/main/kotlin/Class.kt
+++ src/main/kotlin/Class.kt
@@ -1,24 +1,101 @@
 data class Subject(
     val name: String,
+    val hoursPerWeek: Int,
 )
 
 data class Day(
-    val subject: Subject
+    var subjects: MutableList<Subject?>,
+    val className: String,
+    val weekDay: Int,
 ){
+    private val totalHours = 4;
+    private var blankTimes = mutableListOf<Int>()
+
+    private fun hasSubjectInOtherClassAtTime(time: Int, subjectName: String, classes: Array<Class>): Boolean{
+        return  classes.any { classInfo ->
+            classInfo.name != className &&
+                    (classInfo.week[weekDay]?.subjects?.size ?: 0) > time &&
+                    classInfo.week[weekDay]?.subjects?.get(time)?.name == subjectName
+        }
+    }
+    fun addSubject(subject: Subject, classes: Array<Class>): Subject {
+        if(subjects.size >= totalHours && blankTimes.isEmpty()) {
+            return subject;
+        }
+
+        if(subject.hoursPerWeek <= 0){
+            return Subject(subject.name, 0)
+        }
+
+        if(subjects.filter { it?.name == subject.name }.size >= 2) {
+            return subject;
+        }
+
+
+        if(hasSubjectInOtherClassAtTime(time = subjects.size, subjectName = subject.name, classes = classes)) {
+            subjects.add(null);
+            blankTimes.add(subjects.size - 1)
+        }else {
+            if(blankTimes.isNotEmpty() && !hasSubjectInOtherClassAtTime(time = blankTimes.first(), subjectName = subject.name, classes = classes)){
+                subjects[blankTimes.removeAt(0)] = Subject(subject.name, 1)
+                return Subject(subject.name, subject.hoursPerWeek - 1)
+            }else if(subjects.size < totalHours){
+                subjects.add(Subject(subject.name, 1))
+                return addSubject(Subject(subject.name, subject.hoursPerWeek - 1), classes)
+            } else {
+                return subject;
+            }
+        }
+        return addSubject(Subject(subject.name, subject.hoursPerWeek), classes)
+    }
+
     override fun toString(): String {
-        return subject.name;
+        var str = ""
+        subjects.forEachIndexed { index, it ->
+            str = if(index == 3) {
+                str + it?.name + " ---"
+            } else {
+                str + it?.name + " "
+            }
+        }
+        return str;
     }
 }
 
-data class Class(
-    val name: String,
-    val agenda: MutableMap<Int, Day>
-){
-    override fun toString(): String {
-        var agendaStr = "$name: "
-        agenda.forEach{
-            day -> agendaStr+="$day "
+data class Class(val name: String) {
+    val week: Map<Int, Day> = mapOf(
+        0 to Day(mutableListOf(), name, 0),
+        1 to Day(mutableListOf(), name, 1),
+        2 to  Day(mutableListOf(), name, 2),
+        3 to Day(mutableListOf(), name, 3),
+        4 to Day(mutableListOf(), name, 4),
+    )
+
+    fun addSubject(subjects: Array<Subject>, classes: Array<Class>) {
+        val s = subjects.toMutableList()
+        for((index, subject) in s.withIndex()) {
+            var currentSubject = subject;
+            for ((day, agenda) in week) {
+                currentSubject = agenda.addSubject(currentSubject, classes)
+                s[index] = currentSubject
+
+            }
         }
-        return agendaStr;
+    }
+
+    fun getHoursFromWeek(): Int{
+        var totalHours = 0;
+        for((index, agenda) in week){
+            totalHours += agenda.subjects.filterNotNull().size
+        }
+        return totalHours
+    }
+
+    override fun toString(): String {
+        var weekStr = "$name: "
+        week.forEach{
+            day -> weekStr+="$day "
+        }
+        return weekStr;
     }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,54 +1,34 @@
+import java.lang.Exception
+
 fun main(args: Array<String>) {
     populateAgenda()
 }
 
 fun populateAgenda(){
-    val port = Subject(name = "Portugues")
-    val math = Subject(name = "Matematica")
-    val geog = Subject(name = "Geografia")
-    val hist = Subject(name = "Historia")
-    val cien = Subject(name = "Ciencias")
+    val port = Subject(name = "POR", 4)
+    val math = Subject(name = "MAT", 4)
+    val geog = Subject(name = "GEO", 3)
+    val hist = Subject(name = "HIS", 3)
+    val cien = Subject(name = "CIE", 3)
 
-    val class1 = Class("Class1", agenda = mutableMapOf())
-    val class2 = Class("Class2", agenda = mutableMapOf())
-    val class3 = Class("Class3", agenda = mutableMapOf())
-    val class4 = Class("Class4", agenda = mutableMapOf())
+    val class1 = Class("Class1")
+    val class2 = Class("Class2")
+    val class3 = Class("Class3")
+    val class4 = Class("Class4")
 
     val subjects = arrayOf(port, math, geog, hist, cien)
     val classes = arrayOf(class1, class2, class3, class4)
 
-    var currentSubject = 0;
-    var firstSubjectDay = 0
     classes.forEach { c ->
-        var currentDay = firstSubjectDay
-
-        while(currentDay <= subjects.size - 1){
-            if(currentSubject >= subjects.size) break;
-            if(c.agenda[currentDay] == null){
-                c.agenda[currentDay] = Day(subject = subjects[currentSubject])
-                currentSubject++
-                currentDay++;
-            }
-        }
-
-        if(firstSubjectDay > 0){
-            currentDay = 0
-            while (currentDay < firstSubjectDay) {
-                c.agenda[currentDay] = Day(subject = subjects[currentSubject])
-                currentDay++
-                currentSubject++
-            }
-        }
-
-        currentSubject = 0
-        firstSubjectDay++
+        c.addSubject(subjects, classes)
     }
 
     classes.forEach { c ->
-        val sortedAgenda = c.agenda.toSortedMap() // Sort the agenda by day
+        val sortedAgenda = c.week.toSortedMap() // Sort the agenda by day
         val agendaString = sortedAgenda.entries.joinToString(" ") { (day, dayInfo) ->
-            "$day=${dayInfo.subject.name}"
+            "$day=${dayInfo.toString()}"
         }
         println("${c.name}: $agendaString")
+        println("totalHours: ${c.getHoursFromWeek()}")
     }
 }
